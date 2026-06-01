f3ad372a347b9f15b1c18893bd5252700de72281
lyd136688 <1059001342@qq.com>
2026-06-01 23:16:37 +0800

    Update WebViewActivity.kt

--- app/src/main/java/com/localai/chat/WebViewActivity.kt
+++ app/src/main/java/com/localai/chat/WebViewActivity.kt
@@ -4,29 +4,24 @@ import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
-import com.just.agentweb.AgentWeb
 
 class WebViewActivity : AppCompatActivity() {
-    private lateinit var agentWeb: AgentWeb
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_webview)
 
-        agentWeb = AgentWeb.with(this)
-            .setAgentWebParent(findViewById(android.R.id.content), android.widget.LinearLayout.LayoutParams(-1, -1))
-            .useDefaultIndicator()
-            .createAgentWeb()
-            .ready()
-            .go("https://www.baidu.com")
-
-        agentWeb.webCreator.webView?.webViewClient = object : WebViewClient() {
-            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean = false
-        }
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.baidu.com")
     }
 
     override fun onBackPressed() {
-        if (!agentWeb.back()) {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
             super.onBackPressed()
         }
     }
