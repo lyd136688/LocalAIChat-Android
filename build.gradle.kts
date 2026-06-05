b09100ac796cf1ad927c5bc5284caf5082b48fd4
lyd136688 <1059001342@qq.com>
2026-06-05 12:11:16 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
-}
-
 buildscript {
     repositories {
         google()
@@ -19,6 +12,13 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -26,4 +26,3 @@ allprojects {
         maven { url = uri("https://jitpack.io") }
     }
 }
-
